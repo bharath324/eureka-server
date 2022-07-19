b04ed5fa7d746729107c5e9b94860d2a0c6994c2
Bharath Kumar Nallakaluva <bharath.nallakaluva@techmojo.in>
2022-07-19 23:01:02 +0530

    removed tests and changed final name and jar file name

--- src/test/java/com/river/eurekaserver/EurekaServerApplication324Tests.java
+++ src/test/java/com/river/eurekaserver/EurekaServerApplication324Tests.java
@@ -1,13 +1,12 @@
-package com.river.eurekaserver;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EurekaServerApplication324Tests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.river.eurekaserver;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class EurekaServerApplication324Tests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
