c10993e9a400798b34b8330ef148438eda15c5eb
Bharath Kumar Nallakaluva <bharath.nallakaluva@techmojo.in>
2022-07-19 22:29:52 +0530

    Initial commit eureka server

---
+++ src/test/java/com/river/eurekaserver/EurekaServerApplication324Tests.java
@@ -0,0 +1,13 @@
+package com.river.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplication324Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
